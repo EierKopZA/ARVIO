41e1cd54411e9722959e37b0bfcef7c81b155513
ProdigyV21 <ProdigyV21@users.noreply.github.com>
2026-02-26 00:27:05 +0100

    bump version to V1.7.0

--- app/src/main/kotlin/com/arflix/tv/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/kotlin/com/arflix/tv/ui/screens/settings/SettingsScreen.kt
@@ -533,7 +533,7 @@ fun SettingsScreen(
                 Spacer(modifier = Modifier.weight(1f))
                 
                 Text(
-                    text = "ARVIO V1.6",
+                    text = "ARVIO V1.7",
                     style = ArflixTypography.caption,
                     color = TextSecondary.copy(alpha = 0.5f),
                     modifier = Modifier.padding(start = 16.dp)
